fe39d63938521a68f2c2972ca6758a9f839bd404
tshims <bentshims21@gmail.com>
2025-07-20 22:41:16 +0200

    ajout des relations et jointures

--- app/models/contact.ts
+++ app/models/contact.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Contact extends BaseModel {
   @column({ isPrimary: true })
@@ -43,6 +45,10 @@ export default class Contact extends BaseModel {
 
   @column()
   declare addressTypes: string[]
+
+  @column()
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,10 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import Contact from './contact.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -21,6 +23,10 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  @hasMany(() => Contact)
+  declare contacts: HasMany<typeof Contact>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
